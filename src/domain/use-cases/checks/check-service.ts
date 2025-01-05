a3c9619df7e0060958f497bc435231fb1de2395c
DUVAN0829 <duvancasgonz29@gmail.com>
2025-01-04 22:32:49 -0500

    finalizar primera parte usando presentation y use-case

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -1,7 +1,37 @@
-import { promises } from "dns";
 
-export class CheckService {
+interface CheckServiceUseCase {
+    execute(url: string): Promise<boolean>
+}
 
-    async execute(url: string) :promise<boolean>
+type SuccesCallback = () => void
+type ErrorCallback = (error: string) => void
+
+export class CheckService implements CheckServiceUseCase {
+
+    constructor(
+        private readonly succesCallback: SuccesCallback,
+        private readonly errorCallback: ErrorCallback
+    ) { }
+
+    async execute(url: string): Promise<boolean> {
+
+        try {
+
+            const req = await fetch(url)
+
+            if (!req.ok) throw new Error(`Error on check service ${url}`)
+
+            this.succesCallback()
+
+        } catch (error) {
+
+            this.errorCallback(`${error}`)
+            return false
+
+        }
+
+        return true
+
+    }
 
 }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,3 +1,5 @@
+import { error } from "console"
+import { CheckService } from "../domain/use-cases/checks/check-service"
 import { CronService } from "./cron/cron-service"
 
 export class Server {
@@ -6,8 +8,14 @@ export class Server {
         console.log('server started...')
 
         CronService.createJob('*/5 * * * * *', () => {
-            
-            
+
+            const url = 'http://localhost:3000/posts'
+
+            new CheckService(
+                () => console.log(`${url} is ok`),
+                (error) => console.log(error)
+            ).execute(url)
+
         })
 
     }

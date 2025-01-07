7cd9c8fca6294693dcf2635a58c6cee62afa19df
DUVAN0829 <duvancasgonz29@gmail.com>
2025-01-07 07:42:19 -0500

    implementa clase abstracta en 'file-system.datasource' del directorio 'infrastructure'

--- src/infrastructure/datasources/file-sytem.datasource.ts
+++ src/infrastructure/datasources/file-sytem.datasource.ts
@@ -5,7 +5,7 @@ import fs from 'fs'
 export class FileSystemDatasource implements LogDataSource {
 
     private readonly logPath = 'logs/'
-    private readonly allLogsPath = 'logs/logs-low.log'
+    private readonly allLogsPath = 'logs/logs-all.log'
     private readonly mediumLogsPath = 'logs/logs-medium.log'
     private readonly highLogsPath = 'logs/logs-high.log'
 
@@ -26,14 +26,27 @@ export class FileSystemDatasource implements LogDataSource {
             if (fs.existsSync(path)) return
 
             fs.writeFileSync(path, '')
-            
+
         })
 
     }
 
-    saveLog(log: LogEntity): Promise<void> {
-        throw new Error("Method not implemented.");
+    async saveLog(newLog: LogEntity): Promise<void> {
+
+        const logAsJson = `${JSON.stringify(newLog)}\n`
+
+        fs.appendFileSync(this.allLogsPath, logAsJson)
+
+        if (newLog.level === LogSeverityLevel.low) return
+
+        if (newLog.level === LogSeverityLevel.medium) {
+            fs.appendFileSync(this.mediumLogsPath, logAsJson)
+        } else {
+            fs.appendFileSync(this.highLogsPath, logAsJson)
+        }
+
     }
+
     getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
         throw new Error("Method not implemented.");
     }
